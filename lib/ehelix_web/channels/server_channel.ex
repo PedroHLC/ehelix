973f2cbd6c8ca7f974583d14bd0f1f71322e7ce5
kress95 <cferraz95@gmail.com>
2017-08-07 17:12:02 -0300

    Add constant file index.

--- lib/ehelix_web/channels/server_channel.ex
+++ lib/ehelix_web/channels/server_channel.ex
@@ -6,6 +6,52 @@ defmodule EHelixWeb.ServerChannel do
     {:ok, socket}
   end
 
+  def handle_in("file.index", _, socket) do
+    reply =
+      %{
+        data:
+          [
+            %{
+              id: "001",
+              name: "home",
+              children:
+                [
+                  %{
+                    id: "002",
+                    name: "root",
+                    children:
+                      [
+                        %{
+                          id: "003",
+                          name: "Firewall",
+                          extension: "fwl",
+                          size: "900000",
+                          version: "2",
+                          modules: [
+                            %{ name: "Active", version: "1" },
+                            %{ name: "Passive", version: "2" }
+                          ]
+                        },
+                        %{
+                          id: "004",
+                          name: "Virus",
+                          extension: "spam",
+                          size: "752000",
+                          version: "2",
+                          modules: [
+                            %{ name: "Active", version: "1" }
+                          ]
+                        }
+                      ]
+                  }
+                ]
+            }
+          ]
+      }
+
+    {{:ok, reply}, socket}
+  end
+
   defp notify(id, data) do
     EHelixWeb.Endpoint.broadcast("server:" <> id, "event", data)
   end
